60c95e2eac2f72ef95c2e198ecb4a70392fef97a
George Mount <george@stringfestanalytics.com>
2023-10-03 12:05:11 -0400

    upload solutions

---
+++ 1-welcome-to-r-and-rstudio/ch-6-solutions.R
@@ -0,0 +1,27 @@
+# 1. I prefer the Merbivore theme... It's like Halloween year-round!
+
+# 2a. Assign the sum of 1 and 4 as a
+a <- sum(1,4)
+
+# 2b. Assign the square root of a as b
+b <- sqrt(a)
+
+# 2c. Assign b - 1 as d
+d <- b - 1
+
+# 2d. Is d greater than 2?
+d > 2
+
+# 2e. What type of data is stored in d?
+str(d)
+
+# 4. Install the psych package from CRAN.
+# Installing is a one-time process to download
+# the package from CRAN.
+
+# install.packages('psych')
+
+
+# and load it into your session
+# Calling it in needs to be done each time.
+library(psych)
